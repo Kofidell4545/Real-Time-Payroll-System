60eb2f9e6188a3ad6f13e889f2868ae81898d989
Kofidell4545 <davidnhyiraba8@gmail.com>
2025-05-14 19:44:44 +0000

    Fix: Add React hooks imports to PriceTracker

--- client/src/components/employer/PriceTracker.js
+++ client/src/components/employer/PriceTracker.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ethLogo from '../../assets/eth-logo.svg';
 import usdcLogo from '../../assets/usdc-logo.svg';
 import './PriceTracker.css';
